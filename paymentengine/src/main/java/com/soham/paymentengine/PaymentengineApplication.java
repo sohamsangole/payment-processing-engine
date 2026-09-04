82a6d4a0d4f17c13b14be4354ee2ea3f093917d3
Soham Sangole <sasangole2019@gmail.com>
2026-09-04 23:45:09 +0530

    Add initial Spring Boot application setup

---
+++ paymentengine/src/main/java/com/soham/paymentengine/PaymentengineApplication.java
@@ -0,0 +1,13 @@
+package com.soham.paymentengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentengineApplication.class, args);
+	}
+
+}
---
+++ paymentengine/src/test/java/com/soham/paymentengine/PaymentengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soham.paymentengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
