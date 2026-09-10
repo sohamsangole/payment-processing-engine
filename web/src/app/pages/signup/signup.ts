931007d8f02cb6bb9c9e5a0cbb529fc08e085b55
Soham Sangole <sasangole2019@gmail.com>
2026-09-10 18:37:03 +0530

    Implement login and signup pages with form validation and styling

--- web/src/app/pages/signup/signup.ts
+++ web/src/app/pages/signup/signup.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [RouterLink, FormsModule],
   selector: 'app-signup',
   styleUrl: './signup.css',
   templateUrl: './signup.html',
 })
-export class Signup {}
+export class Signup {
+  fullName = '';
+  username = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  showPassword = signal(false);
+
+  togglePassword() {
+    this.showPassword.update((val) => !val);
+  }
+
+  onSubmit() {
+    console.log('Signup form submitted', {
+      fullName: this.fullName,
+      username: this.username,
+      email: this.email,
+    });
+  }
+}
